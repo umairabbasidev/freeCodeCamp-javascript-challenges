6d22e9676a78af4a6dfdf5d29debc5e5cf6a03a3
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-08 17:36:05 +0500

    freeCodeCamp challenge 2 completed

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-// 1 Comment Your JavaScript Code
+// 1) Comment Your JavaScript Code
 
 // This is an in-line comment
 // This is multi-line comment 
+
+// 2) Declare JavaScript Variables
+
+  var myName;
+  
