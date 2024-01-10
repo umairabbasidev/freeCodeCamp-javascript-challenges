0cd8c149af48353a6069b60c924fde1d21e64929
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-10 13:04:21 +0500

    freeCodeCamp challenge No 21,22,23,24 are completed

--- app.js
+++ app.js
@@ -121,3 +121,56 @@ console.log(quotient2)
 const remainder = 11 % 3;
 console.log(remainder)
   
+// 21) Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+
+a += 12; // instead of a = a + 12
+b += 9 ;   
+c += 7;
+// console.log(a,b,c)
+
+// 22)  Compound Assignment With Augmented Substraction
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+
+a -= 6;
+b -= 15;
+c -= 1;
+// console.log(a,b,c)
+
+
+// 23)  Compound Assignment With Augmented multiplication
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+
+
+a*= 5;
+b*= 3;
+c*=10;
+console.log(a,b,c)
+
+
+
+// 24)  Compound Assignment With Augmented division
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
+
+
+console.log(a,b,c)
+
