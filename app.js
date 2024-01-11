ddee903e412caa9cf2cd69d98a1676cb51dffb11
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-11 10:41:31 +0500

    freeCodeCamp challenge No 31 completed

--- app.js
+++ app.js
@@ -223,5 +223,14 @@ myStr += someAdjective
 
 console.log(myStr)*/
 
+// 31) Find the Length of a String
+
+// let lastNameLength = 0;
+// const lastName = "Lovelace";
+
+// // Only change code below this line
+// lastNameLength = lastName.length;
+// console.log(lastNameLength)
+
 
 
