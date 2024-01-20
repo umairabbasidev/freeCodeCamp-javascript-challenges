77ecbb37fd9ef58276af798de7c527e1e4432971
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-01-20 19:20:24 +0500

    project 01_project_RPG -Dragon-Repeller-game Step 70 is completed

--- challengenNo-1-50-app.js
+++ challengenNo-1-50-app.js
@@ -360,3 +360,63 @@ const secondToLastLetterOfLastName = lastName[lastName.length-2];
 //   console.log("Hi World")
 // }
 // reusableFunction()
+
+// 48) Passing Values to Functions with Arguments
+
+// function functionWithArgs(num1,num2){
+//   console.log(num1 + num2)
+//   }
+//   functionWithArgs(5,5)
+
+// 49) Return a Value from a Function with Return
+
+// function timesFive(num){
+//   return num * 5
+//   }
+  
+//   const returnVal = timesFive(5)
+//   console.log(returnVal)
+
+
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  oopsGlobal = 5;
+
+}
+
+
+// 50) Global Scope and Functions
+
+// Only change code above this line
+
+// function fun2() {
+//   let output = "";
+//   if (typeof myGlobal != "undefined") {
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined") {
+//     output += " oopsGlobal: " + oopsGlobal;
+//   }
+//   console.log(output);
+//   // console.log(typeof myGlobal,typeof oopsGlobal)
+// }
+
+
+// 51) Local Scope and Functions
+
+// function myLocalScope() {
+//   // Only change code below this line
+// let myVar =  10;
+//   console.log('inside myLocalScope', myVar);
+
+// }
+// myLocalScope();
+
+// // Run and check the console
+// // myVar is not defined outside of myLocalScope
+// console.log('outside myLocalScope', myVar);
+
