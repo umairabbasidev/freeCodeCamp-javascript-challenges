4be67a4bcb8e33fbc52a944562e82d5b5959a186
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-02-01 22:03:17 +0500

    JS DSA challenges 1-8 are completed

---
+++ basicJavaScript/app.js
@@ -0,0 +1,53 @@
+// 1) Comment Your JavaScript Code
+
+// This is an in-line comment.
+/* This is a
+multi-line comment */
+
+// 2) Declare JavaScript Variables
+
+// var myName;
+
+// 3) Storing Values with the Assignment Operator
+
+// var a;
+// a = 7;
+
+// 4) Assigning the Value of One Variable to Another
+
+// var a;
+// a = 7;
+// var b;
+// b = a;
+
+// 5) Initializing Variables with the Assignment Operator
+
+// var a = 9;
+
+// 6) Declare String Variables
+
+// var myFirstName = "Muhammad Umair";
+// var myLastName = "Abbasi";
+
+// 7) Understanding Uninitialized Variables
+
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+// console.log([a,b,c])
+
+// 8) Understanding Case Sensitivity in Variables
+
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
