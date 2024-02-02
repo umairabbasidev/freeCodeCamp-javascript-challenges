c2065ae4291489ee1edd20de29b123fd31e73deb
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-02-02 18:07:08 +0500

    JS DSA challenges 9-24 are completed

--- basicJavaScript/app.js
+++ basicJavaScript/app.js
@@ -46,8 +46,103 @@ multi-line comment */
 // var properCamelCase;
 // var titleCaseOver;
 
-
 // studlyCapVar = 10;
 // properCamelCase = "A String";
 // titleCaseOver = 9000;
+
+// 9) Explore Differences Between the var and let Keywords
+
+// let catName = "Oliver";
+// let catSound = "Meow!";
+
+// 10)
+
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
+// fact = "is awesome!";
+// console.log(FCC, fact);
+
+// 11) Add Two Numbers with JavaScript
+
+// const sum = 10 + 10;
+
+// 12) Subtract One Number from Another with JavaScript
+
+// const difference = 45 - 33;
+
+// 13) Multiply Two Numbers with JavaScript
+
+// const product = 8 * 10;
+
+// 14) Divide One Number by Another with JavaScript
+
+// const quotient = 66 / 33;
+
+// 15) Increment a Number with JavaScript
+
+// let myVar = 87;
+// myVar++;
+
+// 16) Decrement a Number with JavaScript
+
+// let myVar = 11;
+
+// myVar--;
+
+// 17) Create Decimal Numbers with JavaScript
+
+// let myDecimal = 5.7;
+
+// 18) Multiply Two Decimals with JavaScript
+
+// const product = 2.0 * 2.5;
+
+// 19) Divide One Decimal by Another with JavaScript
+
+// const quotient = 4.4 / 2.0;
+
+// 20) Finding a Remainder in JavaScript
+
+// const remainder = 11 % 3;
+
+// 21) Compound Assignment With Augmented Addition
+
+// let a = 3;
+// let b = 17;
+// let c = 12;
+
+// a += 12;
+// b += 9;
+// c += 7;
+
+// 22) Compound Assignment With Augmented Subtraction
+
+// let a = 11;
+// let b = 9;
+// let c = 3;
+
+// a -= 6;
+// b -= 15;
+// c -= 1;
+
+// 23) Compound Assignment With Augmented Multiplication
+
+// let a = 5;
+// let b = 12;
+// let c = 4.6;
+
+// a *= 5;
+// b *= 3;
+// c *= 10;
+
+// 24) Compound Assignment With Augmented Division
+
+// let a = 48;
+// let b = 108;
+// let c = 33;
+
+// a /= 12;
+// b /= 4;
+// c /= 11;
+
 
