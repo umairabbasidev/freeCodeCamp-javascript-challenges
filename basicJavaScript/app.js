356d1c8a459d690cad0df1e6fb35ec24fc216e53
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-02-10 18:19:20 +0500

    JS DSA challenges till 56 are complted

--- basicJavaScript/app.js
+++ basicJavaScript/app.js
@@ -151,13 +151,13 @@ multi-line comment */
 
 // 26)
 
-// 27) Escape Sequences in Strings 
+// 27) Escape Sequences in Strings
 
 // FirstLine
 //     \SecondLine
 // ThirdLine
 
-// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; 
+// const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 // console.log(myStr)
 
 // 28)  Concatenating Strings with Plus Operator
@@ -190,12 +190,12 @@ multi-line comment */
 
 // let firstLetterOfLastName = "";
 // const lastName = "Lovelace";
-// firstLetterOfLastName = lastName[0]; 
+// firstLetterOfLastName = lastName[0];
 
 // 34) Understand String Immutability
 
 // let myStr = "Jello World";
-// myStr = "Hello World"; 
+// myStr = "Hello World";
 
 // 35) Use Bracket Notation to Find the Nth Character in a String
 
@@ -205,22 +205,21 @@ multi-line comment */
 // 36) Use Bracket Notation to Find the Last Character in a String
 
 // const lastName = "Lovelace";
-// const lastLetterOfLastName = lastName[lastName.length-1]; 
+// const lastLetterOfLastName = lastName[lastName.length-1];
 
 // // 37)  Use Bracket Notation to Find the Nth-to-Last Character in a String
 
 // const lastName = "Lovelace";
 // const secondToLastLetterOfLastName = lastName[lastName.length-2]
 
-// 38) Word Blanks 
+// 38) Word Blanks
 
 // const myNoun = "dog";
 // const myAdjective = "big";
 // const myVerb = "ran";
 // const myAdverb = "quickly";
-// const wordBlanks = myAdjective + " " +  myNoun + " " +myVerb + " "+ myAdverb; 
+// const wordBlanks = myAdjective + " " +  myNoun + " " +myVerb + " "+ myAdverb;
 // console.log(wordBlanks)
-
 
 // 39) Store Multiple Values in one Variable using JavaScript Arrays
 
@@ -235,7 +234,7 @@ multi-line comment */
 // const myArray = [50, 60, 70];
 // const myData = myArray[0]
 
-// 42) Modify Array Data With Indexes 
+// 42) Modify Array Data With Indexes
 // const myArray = [18, 64, 99];
 // myArray[0] = 45;
 
@@ -250,7 +249,6 @@ multi-line comment */
 
 // const myData = myArray[2][1];
 // console.log(myData)
-
 
 // 44) Manipulate Arrays With push Method
 
@@ -281,8 +279,105 @@ multi-line comment */
 // myList.unshift(["laptop",10],["mobiles",20],["watches",30],["chargers",40],["keyboard",50])
 // console.log(myList)
 
+// 49) Write Reusable JavaScript with Functions
+
+// function reusableFunction(){
+//  console.log("Hi World");
+// }
+// reusableFunction()
+
+// 50) Passing Values to Functions with Arguments
+
+// function functionWithArgs(num1,num2){
+//  console.log(num1 + num2)
+//  }
+//  functionWithArgs(5,5)
+
+// 51) Return a Value from a Function with Return
+
+// function timesFive(num){
+//  return num * 5;
+//  }
+
+//  const answer = timesFive(5)
+//  console.log(answer)
+
+// 52) Global Scope and Functions
+
+// let myGlobal = 10;
+
+// function fun1() {
+//   oopsGlobal = 5;
+// }
+
+// function fun2() {
+//   let output = "";
+//   if (typeof myGlobal != "undefined") {
+//     output += "myGlobal: " + myGlobal;
+//   }
+//   if (typeof oopsGlobal != "undefined") {
+//     console.log(typeof oopsGlobal != "undefined");
+//     output += " oopsGlobal: " + oopsGlobal;
+//   }
+//   console.log(output);
+// }
+
+// fun2();
 
 
+// 53) Local Scope and Functions
 
 
+// function myLocalScope() {
+//  // Only change code below this line
+// let myVar = 0;
+//  console.log('inside myLocalScope', myVar);
+// }
+// myLocalScope();
 
+// // Run and check the console
+// // myVar is not defined outside of myLocalScope
+// console.log('outside myLocalScope', myVar);
+
+// 54) Global vs. Local Scope in Functions
+
+// It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+
+// const outerWear = "T-Shirt";
+
+// function myOutfit() {
+// const outerWear = "sweater";
+//   return outerWear;
+// }
+
+// myOutfit();
+
+
+// 55) Understanding Undefined Value returned from a Function
+
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// // Only change code below this line
+
+// function addFive(){
+//   sum+=5
+//   console.log(sum)
+// }
+// // Only change code above this line
+
+// addThree();
+
+
+// 56) Assignment with a Returned Value
+
+// let processed = 0;
+
+// function processArg(num) {
+//   return (num + 3) / 5;
+// }
+// processed =  processArg(7)
+// console.log(processed)
