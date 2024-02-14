d262b95e7170404ca016b792588bbe2e694a9930
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-02-14 09:28:45 +0500

    JS DSA challges till 68 are completed

--- basicJavaScript/app.js
+++ basicJavaScript/app.js
@@ -453,3 +453,95 @@ multi-line comment */
 // console.log(compareEquality(10, "10"))
 // console.log(compareEquality(20, "20"))
 // console.log(compareEquality(10, 10))
+
+// 63) Comparison with the Inequality Operator
+
+// function testNotEqual(val) {
+//  if (val != 99) { // Change this line
+//    return "Not Equal";
+//  }
+//  return "Equal";
+// }
+
+// console.log(testNotEqual(99));
+
+
+// 64) Comparison with the Strict Inequality Operator
+
+// Setup
+// function testStrictNotEqual(val) {
+//  if (val !== 17) { // Change this line
+//    return "Not Equal";
+//  }
+//  return "Equal";
+// }
+
+// console.log(testStrictNotEqual(17));
+
+
+// 65)  Comparison with the Greater Than Operator
+
+// function testGreaterThan(val) {
+//  if (val > 100) {  // Change this line
+//    return "Over 100";
+//  }
+
+//  if (val > 10) {  // Change this line
+//    return "Over 10";
+//  }
+
+//  return "10 or Under";
+// }
+
+// console.log(testGreaterThan(11));
+
+
+// 66) Comparison with the Greater Than Or Equal To Operator
+
+// function testGreaterOrEqual(val) {
+//  if (val >= 20) {  // Change this line
+//    return "20 or Over";
+//  }
+
+//  if (val >= 10) {  // Change this line
+//    return "10 or Over";
+//  }
+
+//  return "Less than 10";
+// }
+
+// console.log(testGreaterOrEqual(100));
+
+
+// 67) Comparison with the Less Than Operator
+
+// function testLessThan(val) {
+//  if (val < 25) {  // Change this line
+//    return "Under 25";
+//  }
+
+//  if (val < 55) {  // Change this line
+//    return "Under 55";
+//  }
+
+//  return "55 or Over";
+// }
+
+// console.log(testLessThan(10));
+
+// 68) Comparison with the Less Than Or Equal To Operator
+
+// function testLessOrEqual(val) {
+//  if (val <= 12) {  // Change this line
+//    return "Smaller Than or Equal to 12";
+//  }
+
+//  if (val <= 24) {  // Change this line
+//    return "Smaller Than or Equal to 24";
+//  }
+
+//  return "More Than 24";
+// }
+
+// testLessOrEqual(10);
+
