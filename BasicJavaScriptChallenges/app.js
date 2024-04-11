fbd9e71fed94f09f21eceeb3048a35ad880a052c
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-04-11 09:23:39 +0500

    first 10 DSA with javascript challenges are completed

---
+++ BasicJavaScriptChallenges/app.js
@@ -0,0 +1,66 @@
+// console.log("Starting DSA With Javascript");
+
+// 1) Comment Your JavaScript Code
+
+// This is single line comment
+
+/* This is a 
+multi-line comment */
+
+// 2) Declare JavaScript Variables
+
+// var myName;
+
+// 3) Storing Values with the Assignment Operator
+
+// var a; // variable declaration
+// a = 7; // varable initialization
+
+// 4) Assigning the Value of One Variable to Another
+
+/* var a;
+a = 7;
+var b;
+b = a; */
+
+// 5) Initializing Variables with the Assignment Operator
+
+// var a = 9;
+
+// 6) Declare String Variables
+
+// var myFirstName = "Muhammad Umair";
+// var myLastName = "Abbasi";
+
+// 7) Understanding Uninitialized Variables
+
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// 8) Understanding Case Sensitivity in Variables
+
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// 9 Explore Differences Between the var and let Keywords
+
+// let catName = "Oliver";
+// let catSound = "Meow!";
+
+// 10) Declare a Read-Only Variable with the const Keyword
+
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
+// fact = "is awesome!";
+// console.log(FCC, fact);
